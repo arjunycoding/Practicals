f96b6e8195643cda86fb0d3062eb55aa868d9ec6
arjunyuvaraj <arjunycoding@gmail.com>
2021-03-02 20:03:47 -0500

    fixing errors in the for loop

--- census/main.js
+++ census/main.js
@@ -3,9 +3,9 @@ function askMemberInfo(){
     let requestOfInfo = document.getElementById("requestOfInfo"); 
     let i = 1;
     while(i <= numOfMembers){
-        requestOfInfo.innerHTML += ` First Name : <input type = "text" name = "memberFn${i}" id = "memberFn${i}"/> `;
-        requestOfInfo.innerHTML += ` Last Name : <input type = "text" name = "memberLn${i}" id = "memberLn${i}"/> `;
-        requestOfInfo.innerHTML += ` Age : <input type = "number" name = "memberAge${i}" id = "memberAge${i}"/> `;
+        requestOfInfo.innerHTML += ` First Name : <input type = "text" name = "memberFn${i}"  class = "info" id = "memberFn${i}"/> `;
+        requestOfInfo.innerHTML += ` Last Name : <input type = "text" name = "memberLn${i}" class = "info" id = "memberLn${i}"/> `;
+        requestOfInfo.innerHTML += ` Age : <input type = "number" name = "memberAge${i}" class = "info" id = "memberAge${i}"/> `;
         requestOfInfo.innerHTML += `<br/><br/>`;
         i++;
     }
@@ -15,8 +15,12 @@ function askMemberInfo(){
 
 function displayFamilyInfo(){
     let numOfMembers = document.getElementById("numOfMembers").value; 
-    let familyInfoContent = document.getElementById("familyInfoContent");   
-    for(let i = 1; i <= numOfMembers; i++){
-        familyInfoContent.innerHTML +=  document.getElementById(`memberFn${i}`).value + document.getElementById(`memberLn${i}`).value + ' is '  + document.getElementById(`memberAge${i}`).value + ' years old. <br/>  <br/>';
+    let familyInfoContent = document.getElementById("familyInfoContent");  
+    let familyInfoArray = document.getElementsByClassName("info"); 
+    for(let i = 1; i <= familyInfoArray.length; i++){
+        let fn = document.getElementById(`memberFn${i}`).value;
+        let ln = document.getElementById(`memberLn${i}`).value;
+        let age = document.getElementById(`memberAge${i}`).value;
+        familyInfoContent.innerHTML +=  `${fn} ${ln} is ${age} years old. <br/><br/>`;
     }
 }
